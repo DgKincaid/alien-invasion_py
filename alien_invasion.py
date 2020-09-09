4a1eaba144542334bc853a15a7311c409563d9a6
Douglas Kincaid <DgKincaid@users.noreply.github.com>
2020-09-09 11:46:42 -0400

    moving aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -91,6 +91,18 @@ class AlienInvasion:
     elif event.key == pygame.K_LEFT:
       self.ship.moving_left = False
 
+  def _check_fleet_edges(self):
+    for alien in self.aliens.sprites():
+      if alien.check_edges():
+        self._change_fleet_direction()
+        break
+
+  def _change_fleet_direction(self):
+    for alien in self.aliens.sprites():
+      alien.rect.y += self.settings.fleet_drop_speed
+
+    self.settings.fleet_direction *= -1
+
   def _fire_bullet(self):
     if(len(self.bullets) < 3):
       new_bullet = Bullet(self)
@@ -115,6 +127,7 @@ class AlienInvasion:
         self.bullets.remove(bullet)
 
   def _update_aliens(self):
+    self._check_fleet_edges()
     self.aliens.update()
 
 if __name__ == '__main__':
